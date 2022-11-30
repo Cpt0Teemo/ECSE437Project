25525cdcdcabb1caab36264ae7a6a608bd6c63ef
Vy-KhaHuynh <vy-kha.huynh@mail.mcgill.ca>
2022-11-29 21:11:04 -0500

    Added some trig identities
    Change-Id: Ie60ce1db0b6ec082885e65c3825d419bce06a331

--- trigonometry.c
+++ trigonometry.c
@@ -13,3 +13,15 @@ double cos(double rad){
 double tan(double rad){
     return (sin(rad)/cos(rad));
 }
+
+double cosec(double rad){
+    return (power(sin(rad),-1));
+}
+
+double sec(double rad){
+    return (power(cos(rad),-1));
+}
+
+double cotan(double rad){
+    return (power(tan(rad),-1));
+}
