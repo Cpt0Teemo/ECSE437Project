2623212a1b350b5eefdd62ef8493d16f8d625e6e
Cpt0Teemo <ypoulmarck@gmail.com>
2022-12-04 13:42:46 -0500

    Added test and yaml to run tests
    Change-Id: Id877c786b4d9e3b36ff15873b92affaca122ccd8

--- src/test/com/ecse437project/tests/DivisionTest.java
+++ src/test/com/ecse437project/tests/DivisionTest.java
@@ -1,11 +1,59 @@
 package com.ecse437project.tests;
 
+import com.ecse437project.Division;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DivisionTest {
 
     @Test
-    void test() {
+    void shouldDivideTwoPositiveNumbers() {
+        int x = 5;
+        int y = 3;
 
+        int result = Division.apply(x, y);
+
+        int expected = x / y;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldDivideTwoNegativeNumbers() {
+        int x = -5;
+        int y = -3;
+
+        int result = Division.apply(x, y);
+
+        int expected = x / y;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldDivideOneNegativeNumber() {
+        int x = -5;
+        int y = 3;
+
+        int result = Division.apply(x, y);
+
+        int expected = x / y;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldBreakWhenDividingByZero() {
+        int x = 5;
+
+        assertThrows(IllegalStateException.class ,() -> Division.apply(x, 0));
+    }
+
+    @Test
+    void shouldDivideZero() {
+        int x = 5;
+
+        int result = Division.apply(0, x);
+
+        assertEquals(0, result);
     }
 }
