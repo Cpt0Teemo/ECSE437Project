d4d552a50679b7260a4d1d2fbcf664280d04effd
Vy-KhaHuynh <vy-kha.huynh@mail.mcgill.ca>
2022-12-05 16:56:02 -0500

    Added looping for exceptions

--- src/main/java/com/ecse437project/Calculator.java
+++ src/main/java/com/ecse437project/Calculator.java
@@ -1,9 +1,21 @@
 package com.ecse437project;
 
 import java.lang.invoke.MutableCallSite;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
+    final static ArrayList<String> valid = new ArrayList<String>() {
+        {
+            add("add");
+            add("sub");
+            add("multi");
+            add("div");
+            add("pow");
+            add("binomial");
+            add("exit");
+        }
+    };
     static int operation(String op,int x, int y){
         switch (op){
             case "add":
@@ -28,15 +40,41 @@ public class Calculator {
                 throw new IllegalArgumentException("Please enter a valid operator.");
         }
     }
-    public static void main(String[] args) {
+    static int calculate(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a valid operator (add, sub, multi, div, pow, binomial)");
+
+        System.out.println("\nPlease input a valid operator (add,sub, multi, pow, div, exit).");
         String op = scanner.nextLine();
+        while(!valid.contains(op)) {
+            System.err.println("Please enter a valid operator.");
+            op = scanner.nextLine();
+        }
+
+        if (op.equals("exit")) {
+            scanner.close();
+            System.exit(0);
+        }
+
         System.out.println("Please input the first operand.");
         int x = scanner.nextInt();
+
         System.out.println("Please input the second operand.");
         int y = scanner.nextInt();
 
-        System.out.println("\nThe result is "+operation(op,x,y));
+        return operation(op,x,y);
+    }
+    public static void main(String[] args) {
+        while(true) {
+            try {
+                System.out.println("\nThe result is " + calculate());
+            } catch (Exception exception){
+                if(exception.getMessage() == null){
+                    System.err.println("Please input a valid integer");
+                }
+                else {
+                    System.err.println(exception.getMessage());
+                }
+            }
+        }
     }
 }
